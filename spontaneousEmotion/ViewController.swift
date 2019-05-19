af1ede3a55f9f6382484908a69d362cc15cd8088
zorro <zorro@generasicipta.com>
2019-05-19 18:14:48 +0700

    Initial Commit

---
+++ spontaneousEmotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spontaneousEmotion
+//
+//  Created by Zorro Pandiangan on 19/05/19.
+//  Copyright © 2019 Zorro Pandiangan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
